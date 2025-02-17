0b5b28d2331d5f0855e1c8486d29fc3b036e73e5
geneciSI <genecicouto@id.uff.br>
2025-02-16 21:03:27 -0300

    Uso do set LinkedHashSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,17 @@
 package application;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Set<String> set = new TreeSet<>(); // ordena os dados
+		Set<String> set = new LinkedHashSet<>(); // mantem a ordem conforme os dados foram inseridos
 		
 		set.add("Tv");
 		set.add("Tablet");
 		set.add("Notebook");
+		set.add("Motherboard");
 		
 		System.out.println(set.contains("Notebook"));
 		for (String p : set) {
